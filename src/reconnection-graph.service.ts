14f0af01799bf6c165445585aa332c8e488ecc30
John Mela <john.n.mela@gmail.com>
2023-06-21 10:47:26 -0400

    Add api bootstrapping

--- src/reconnection-graph.service.ts
+++ src/reconnection-graph.service.ts
@@ -2,12 +2,45 @@
 https://docs.nestjs.com/providers#services
 */
 
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnApplicationBootstrap, OnApplicationShutdown } from '@nestjs/common';
 import { ReconnectionConfigService } from './reconnection-config.service';
+import { options } from "@frequency-chain/api-augment";
+import { ApiPromise, HttpProvider, WsProvider } from '@polkadot/api';
+import { u64 } from "@polkadot/types"
 
 @Injectable()
-export class ReconnectionGraphService {
-    constructor(private configService: ReconnectionConfigService) {};
+export class ReconnectionGraphService implements OnApplicationBootstrap, OnApplicationShutdown {
+    private api: ApiPromise;
+    private logger: Logger;
+    private type DsnpUserId = u64;
+
+    constructor(private configService: ReconnectionConfigService) {
+        this.logger = new Logger(ReconnectionGraphService.name);
+    };
+
+    async onApplicationBootstrap() {
+        const chainUrl = this.configService.frequencyUrl;
+        let provider: any;
+        if (/^ws/.test(chainUrl.toString())) {
+            provider = new WsProvider(chainUrl.toString());
+        } else if (/^http/.test(chainUrl.toString())) {
+            provider = new HttpProvider(chainUrl.toString());
+        } else {
+            this.logger.error(`Unrecognized chain URL type: ${chainUrl.toString()}`);
+            throw "Unrecognized chain URL type";
+        }
+        this.api = await ApiPromise.create({ provider, ...options });
+        await this.api.isReady;
+        this.logger.log('Blockchain API ready.');
+    }
+
+    async onApplicationShutdown() {
+        await this.api.disconnect();
+    }
+
+    update_user_graph(dsnpUserId: DsnpUserId, providerId: ProviderId, update_connections: bool) {
+
+    }
 
     // TODO
     // Calling out to the provider to obtain a user's Provider graph
