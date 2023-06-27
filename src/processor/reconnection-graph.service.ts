c8f09a4ae98d8bfc03b5602f940f07415ed0bc34
Joe Caputo <joseph.caputo@unfinished.com>
2023-06-27 15:16:19 -0400

    Add queue processor and dev endpoints (#30)
    Adds a queue consumer service for processing graph update request
    events.
    Also adds a development controller with some useful endpoints for
    testing.

---
+++ src/processor/reconnection-graph.service.ts
@@ -0,0 +1,60 @@
+/*
+https://docs.nestjs.com/providers#services
+*/
+
+import { Injectable, Logger, OnApplicationBootstrap, OnApplicationShutdown } from '@nestjs/common';
+import { options } from '@frequency-chain/api-augment';
+import { ApiPromise, HttpProvider, WsProvider } from '@polkadot/api';
+import { MessageSourceId, ProviderId } from '@frequency-chain/api-augment/interfaces';
+import { ConfigService } from '../config/config.service';
+
+@Injectable()
+export class ReconnectionGraphService implements OnApplicationBootstrap, OnApplicationShutdown {
+  private api: ApiPromise;
+
+  private logger: Logger;
+
+  constructor(private configService: ConfigService) {
+    this.logger = new Logger(ReconnectionGraphService.name);
+  }
+
+  async onApplicationBootstrap() {
+    const chainUrl = this.configService.frequencyUrl;
+    let provider: any;
+    if (/^ws/.test(chainUrl.toString())) {
+      provider = new WsProvider(chainUrl.toString());
+    } else if (/^http/.test(chainUrl.toString())) {
+      provider = new HttpProvider(chainUrl.toString());
+    } else {
+      this.logger.error(`Unrecognized chain URL type: ${chainUrl.toString()}`);
+      throw new Error('Unrecognized chain URL type');
+    }
+    this.api = await ApiPromise.create({ provider, ...options });
+    await this.api.isReady;
+    this.logger.log('Blockchain API ready.');
+  }
+
+  async onApplicationShutdown() {
+    await this.api.disconnect();
+  }
+
+  public async updateUserGraph(dsnpUserStr: string, providerStr: string, updateConnections: boolean): Promise<void> {
+    this.logger.debug(`Updating graph for user ${dsnpUserStr}, provider ${providerStr}`);
+    const dsnpUserId: MessageSourceId = this.api.registry.createType('MessageSourceId', dsnpUserStr);
+    const providerId: ProviderId = this.api.registry.createType('ProviderId', providerStr);
+    // TODO
+    // https://github.com/AmplicaLabs/reconnection-service/issues/20
+    // Calling out to the provider to obtain a user's Provider graph
+    // https://github.com/AmplicaLabs/reconnection-service/issues/21
+    // Calling out to the blockchain to obtain the user's DSNP Graph
+    // Import the DSNP Graph into GraphSDK
+    // https://github.com/AmplicaLabs/reconnection-service/issues/22
+    // Adding missing connections to the user's DSNP Graph using GraphSDK API
+    // Export DSNP Graph changes and send to blockchain
+    // Re-import DSNP Graph from chain & verify
+    //     (if updating connections as well, do the same for connections--but do not transitively update connections - of - connections)
+  }
+
+  // TODO define interfaces for the request / response to / from the provider webhook
+  // async get_user_graph_from_provider(dsnpUserId: u64): {}
+}
--- src/reconnection-service.controller.ts
+++ src/reconnection-service.controller.ts
@@ -1,18 +1,44 @@
-/*
-https://docs.nestjs.com/controllers#controllers
-*/
+import { Controller, Get, HttpException, HttpStatus, Logger, Param, Post } from '@nestjs/common';
+import { InjectQueue } from '@nestjs/bullmq';
+import { Queue } from 'bullmq';
+import { BlockchainScannerService } from './blockchain-scanner.service';
 
-import { Controller, Get, Logger } from '@nestjs/common';
+type JobStatus = 'active' | 'completed' | 'failed' | 'delayed';
 
 @Controller('reconnection-service')
 export class ReconnectionServiceController {
   private readonly logger: Logger;
 
-  constructor() {
+  constructor(private scannerService: BlockchainScannerService, @InjectQueue('graphUpdateQueue') private graphUpdateQueue: Queue) {
     this.logger = new Logger(this.constructor.name);
   }
 
   // eslint-disable-next-line class-methods-use-this
   @Get('health')
   health() {}
+
+  @Get('queue')
+  async queue() {
+    return this.graphUpdateQueue.getJobCounts();
+  }
+
+  @Get('queue/:jobstatus')
+  async getQueueByStatus(@Param('jobstatus') jobstatus: JobStatus) {
+    switch (jobstatus) {
+      case 'active':
+        return this.graphUpdateQueue.getActive();
+
+      case 'completed':
+        return this.graphUpdateQueue.getCompleted();
+
+      case 'delayed':
+        return this.graphUpdateQueue.getDelayed();
+
+      case 'failed':
+        return this.graphUpdateQueue.getFailed();
+
+      default:
+        throw new HttpException('Unrecognized job status', HttpStatus.BAD_REQUEST);
+    }
+  }
 }
