8eef1d83fa9484f39beab15f5d4c56cf76333768
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-06-27 17:51:23 -0500

    add comments

--- src/graph/graph-state.module.ts
+++ src/graph/graph-state.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
 import { GraphStateManager } from './graph-state-manager';
 import { createGraphStateManager } from './graph-state-manager.factory';
+import { EnvironmentType } from '@dsnp/graph-sdk';
 
 @Module({
     imports: [],
     providers: [
         {
             provide: GraphStateManager,
-            useFactory: createGraphStateManager,
+            useFactory: () => {
+                // Replace with env needed for testing as default is mainnet
+                // var env = { environmentType: EnvironmentType.Mainnet };
+                createGraphStateManager();
+            },
         },
     ],
     exports: [GraphStateManager],
