7441b360af90e364b6362a8aac1574855bb12a86
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-06-28 15:42:58 -0500

    add tests

--- src/graph/graph-state-manager.spec.ts
+++ src/graph/graph-state-manager.spec.ts
@@ -1,14 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GraphStateManager } from './graph-state-manager';
-import { GraphManagerModule } from './graph-state.module';
-import { DsnpKeys, GraphKeyPair, ImportBundle, PageData } from '@dsnp/graph-sdk';
+import { DsnpKeys, EnvironmentType, GraphKeyPair, ImportBundle, PageData } from '@dsnp/graph-sdk';
+import { ConfigModule } from '../config/config.module';
 
 describe('GraphStateManager', () => {
   let graphStateManager: GraphStateManager;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [GraphManagerModule],
+      imports: [],
+      providers: [{
+        provide: GraphStateManager,
+        useFactory: () => {
+          return new GraphStateManager({ environmentType: EnvironmentType.Mainnet });
+        }
+      }],
     }).compile();
 
     graphStateManager = module.get<GraphStateManager>(GraphStateManager);
