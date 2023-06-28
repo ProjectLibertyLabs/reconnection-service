73fd0502b5734d7388c667348b08a36fa909f014
Puneet Saraswat <puneet.saraswat10074@gmail.com>
2023-06-28 10:09:02 -0500

    cleanup single state pattern

--- src/graph/graph-state-manager.ts
+++ src/graph/graph-state-manager.ts
@@ -3,121 +3,73 @@ import { Action, Graph, EnvironmentInterface, GraphKeyPair, GraphKeyType, Import
 
 @Injectable()
 export class GraphStateManager {
-  private graphStates: Map<number, Graph>; // Map to store multiple graph states
-
-  private userToStatesMap: Map<string, number[]>; // Map to store user to state mapping
-
-  private currentStateId: number; // Current state ID
+  private graphState: Graph;
 
   private environment: EnvironmentInterface; // Environment details
 
   private capacity?: number; // Graph capacity
 
+  private static graphStateFinalizer = new FinalizationRegistry((graphState: Graph) => {
+    if (graphState) {
+      console.log('Finalizing graph state');
+      graphState.freeGraphState(); // Assuming there is a method freeGraphState() that frees up resources in Graph.
+    }
+  });
+
   constructor(environment: EnvironmentInterface, capacity?: number) {
-    this.graphStates = new Map<number, Graph>();
-    this.userToStatesMap = new Map<string, number[]>();
-    this.currentStateId = 1; // Initial state ID
     this.environment = environment;
     this.capacity = capacity;
-  }
+    this.graphState = new Graph(this.environment, this.capacity);
 
-  private generateStateId(): number {
-    const stateId = this.currentStateId;
-    this.currentStateId += 1;
-    return stateId;
-  }
-
-  private async createGraphState(dsnpUserId: string): Promise<Graph> {
-    const graph = new Graph(this.environment, this.capacity);
-    const stateId = this.generateStateId();
-    this.graphStates.set(stateId, graph);
-    const stateIds = this.userToStatesMap.get(dsnpUserId) ?? [];
-    this.userToStatesMap.set(dsnpUserId, [...stateIds, stateId]);
-    return graph;
+    GraphStateManager.graphStateFinalizer.register(this, this.graphState);
   }
 
   private async isGraphStateFull(stateId: number): Promise<boolean> {
-    const graph = this.graphStates.get(stateId);
-    if (graph) {
-      const graphCapacity = await graph.getGraphCapacity();
-      const graphStatesCount = await graph.getGraphUsersCount();
+    if (this.graphState) {
+      const graphCapacity = await this.graphState.getGraphCapacity();
+      const graphStatesCount = await this.graphState.getGraphUsersCount();
       return graphCapacity === graphStatesCount;
     }
     return false;
   }
 
   public async getGraphConfig(stateId?: number): Promise<Config> {
-    const graph = this.graphStates.get(stateId ?? this.graphStates.size);
-    if (graph) {
-      return graph.getGraphConfig(this.environment);
+    if (this.graphState) {
+      return this.graphState.getGraphConfig(this.environment);
     }
     return {} as Config;
-  }
-
-  public async getTotalStatesCount(): Promise<number> {
-    return this.graphStates.size;
   }
 
   public static async generateKeyPair(keyType: GraphKeyType): Promise<GraphKeyPair> {
     return Graph.generateKeyPair(keyType);
   }
 
-  public async getAllStatesForUser(dsnpUserId: string): Promise<number[] | undefined> {
-    return this.userToStatesMap.get(dsnpUserId);
-  }
 
   public async importUserData(dsnpUserId: string, payload: ImportBundle[]): Promise<boolean> {
-    let stateIds = this.userToStatesMap.get(dsnpUserId) ?? [];
-    var currentGraph : Graph | undefined;
-
-    if (stateIds.length === 0 || (await this.isGraphStateFull(stateIds[stateIds.length - 1]))) {
-      currentGraph = await this.createGraphState(dsnpUserId);
-    } else {
-      currentGraph = this.graphStates.get(stateIds[stateIds.length - 1]);
-    }
-
-    if (currentGraph) {
-      return currentGraph.importUserData(payload);
+    if (this.graphState) {
+      return this.graphState.importUserData(payload);
     }
     return false;
   }
 
   public async applyActions(dsnpUserId: string, actions: Action[]): Promise<boolean> {
-    const stateIds = this.userToStatesMap.get(dsnpUserId);
-    if (stateIds) {
-      const lastStateId = stateIds[stateIds.length - 1];
-      const graph = this.graphStates.get(lastStateId);
-      if (graph) {
-        return graph.applyActions(actions);
-      }
+    if (this.graphState) {
+      return this.graphState.applyActions(actions);
     }
     return false;
   }
 
   public async exportGraphUpdates(dsnpUserId: string): Promise<Update[]> {
-    const stateIds = this.userToStatesMap.get(dsnpUserId);
-    let updates: Update[] = [];
-    if (stateIds) {
-      for (const stateId of stateIds) {
-        const graph = this.graphStates.get(stateId);
-        if (graph) {
-          const graphUpdates = await graph.exportUpdates();
-          updates = [...updates, ...graphUpdates];
-        }
-      }
+    if (this.graphState) {
+      return await this.graphState.exportUpdates();
     }
-    return updates;
+    return [];
   }
 
-  public removeGraphState(stateId: number): void {
-    this.graphStates.get(stateId)?.freeGraphState();
-    this.graphStates.delete(stateId);
-  }
-
-  public async clearAllGraphStates(): Promise<void> {
-    this.graphStates.forEach((graph) => {
-      graph.freeGraphState();
-    });
-    this.graphStates.clear();
+  public async removeUserGraph(dsnpUserId: string): Promise<boolean> {
+    if (this.graphState) {
+      return this.graphState.removeUserGraph(dsnpUserId);
+    }
+    return false;
   }
 }
